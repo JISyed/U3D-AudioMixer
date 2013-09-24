e09261680098efda2a153d094f8f86313c6d7051
Jibran Ibrahim Syed <jisyed14@gmail.com>
2013-09-23 22:18:40 -0500

    Added test sounds and started AudioMixer script

---
+++ AudioMixerProj/Assets/U3D-AudioMixer/Scripts/AudioMixer.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioMixer : MonoBehaviour 
+{
+	public const int NUM_OF_CHANNELS = 8;
+	private AudioSource[] channels;
+	
+	// Initilizer
+	void Start () 
+	{
+		// Create the audio channels
+		channels = new AudioSource[NUM_OF_CHANNELS];
+		for(int i=0; i<NUM_OF_CHANNELS; i++)
+		{
+			channels[i] = gameObject.AddComponent<AudioSource>();
+		}
+	}
+	
+	// Update loop
+	void Update () 
+	{
+		
+	}
+	
+	// Used only to get a Gizmo icon in the editor
+	void OnDrawGizmos(){}
+}
